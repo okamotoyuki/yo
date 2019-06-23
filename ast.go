83a7095d1b9e005018eebbe03e46773db83b705d
OkamotoYuki <okamoto.yuki.0130@gmail.com>
2019-06-23 18:04:06 +0900

    support unary operator

--- ast.go
+++ ast.go
@@ -30,22 +30,35 @@ func term(tokens []*Token, pos int) (*Node, int) {
 		return &Node{nodeNum, "number", nil, nil, tokens[pos].val}, next
 	}
 
-	exitWithError("unexpected token in this context. (token.ty: %d)", tokens[pos].ty)
+	exitWithError("unexpected token in this context. (token.ty: %s)", string(tokens[pos].ty))
 	return nil, -1
 }
 
-// mul  = term ("*" term | "/" term)*
+// unary = ("+" | "-")? term
+func unary(tokens []*Token, pos int) (*Node, int) {
+	if next := consume(tokens, pos, '+'); next > pos {
+		return term(tokens, next)
+	} else if next = consume(tokens, pos, '-'); next > pos {
+		lhs := &Node{nodeNum, "number", nil, nil, 0}
+		rhs, pos := term(tokens, next)
+		return &Node{nodeSub, "sub", lhs, rhs, 0}, pos
+	}
+
+	return term(tokens, pos)
+}
+
+// mul  = unary ("*" unary | "/" unary)*
 func mul(tokens []*Token, pos int) (*Node, int) {
-	node, pos := term(tokens, pos)
+	node, pos := unary(tokens, pos)
 
 	var rhs *Node
 
 	for {
 		if next := consume(tokens, pos, '*'); next > pos {
-			rhs, pos = term(tokens, next)
+			rhs, pos = unary(tokens, next)
 			node = &Node{nodeMul, "mul", node, rhs, 0}
 		} else if next = consume(tokens, pos, '/'); next > pos {
-			rhs, pos = term(tokens, next)
+			rhs, pos = unary(tokens, next)
 			node = &Node{nodeDiv, "div", node, rhs, 0}
 		} else {
 			break
